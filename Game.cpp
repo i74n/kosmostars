32857c001d9b348111c861671764ae9b62ab4a3f
Ivan <i74n@ya.ru>
2018-12-28 01:56:17 +0300

    initializing VSC++ project

---
+++ Game.cpp
@@ -0,0 +1,6 @@
+#include "Game.h"
+
+Game::Game(){}
+Game::~Game(){}
+
+void Game::play(){}
---
+++ Game.h
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Game{
+public:
+	Game();
+	~Game();
+	void play();
+};
